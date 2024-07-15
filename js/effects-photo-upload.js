135cbce7b112b21aa81ed9ce961ed7ae94c85a15
Valeria <valeri.kiass.love@gmail.com>
2024-07-15 19:58:32 +0300

    добавлено наложение эффектов и слайдер

---
+++ js/effects-photo-upload.js
@@ -0,0 +1,124 @@
+const uploadImage = document.querySelector('.img-upload__preview img');
+const sliderContainer = document.querySelector('.img-upload__effect-level');
+const effectsContainer = document.querySelector('.effects__list');
+const sliderElement = sliderContainer.querySelector('.effect-level__slider');
+const sliderValue = sliderContainer.querySelector('.effect-level__value');
+
+const Effects = {
+  NONE: 'none',
+  SEPIA: 'sepia',
+  CHROM: 'chrome',
+  MARVIN: 'marvin',
+  PHOBOS: 'phobos',
+  HEAT: 'heat'
+};
+
+const effectsToFilterName = {
+  [Effects.NONE]: 'none',
+  [Effects.SEPIA]: 'sepia',
+  [Effects.CHROM]: 'grayscale',
+  [Effects.MARVIN]: 'invert',
+  [Effects.PHOBOS]: 'blur',
+  [Effects.HEAT]: 'brightness',
+};
+
+const EFFECT_CONFIG = {
+  [Effects.NONE]:  {
+    range: {
+      min: 0,
+      max: 1,
+    },
+    start: 1,
+    step: 1,
+  },
+  [Effects.SEPIA]:  {
+    range: {
+      min: 0,
+      max: 1,
+    },
+    start: 1,
+    step: 0.1,
+  },
+  [Effects.CHROM]:  {
+    range: {
+      min: 0,
+      max: 1,
+    },
+    start: 1,
+    step: 0.1,
+  },
+  [Effects.MARVIN]:  {
+    range: {
+      min: 0,
+      max: 100,
+    },
+    start: 100,
+    step: 1,
+  },
+  [Effects.PHOBOS]:  {
+    range: {
+      min: 0,
+      max: 3,
+    },
+    start: 3,
+    step: 0.1,
+  },
+  [Effects.HEAT]:  {
+    range: {
+      min: 0,
+      max: 3,
+    },
+    start: 3,
+    step: 0.1,
+  },
+};
+
+noUiSlider.create(sliderElement, {...EFFECT_CONFIG[Effects.NONE], connect: 'lower'});
+
+const setEffect = (currentEffect) => {
+  uploadImage.classList.remove(`effects__preview--${uploadImage.dataset.id}`);
+  uploadImage.classList.add(`effects__preview--${currentEffect}`);
+  uploadImage.dataset.id = currentEffect;
+
+  sliderElement.noUiSlider.updateOptions(EFFECT_CONFIG[currentEffect]);
+};
+
+const onEffectClick = (evt) => {
+  if (evt.target.classList.contains('effects__radio')) {
+    const currentEffect = evt.target.value;
+    setEffect(currentEffect);
+  }
+};
+
+const resetEffect = () => {
+  setEffect(Effects.NONE);
+};
+
+const getUnitsEffect = (currentEffect) => {
+  switch (currentEffect) {
+    case 'marvin': return '%';
+    case 'phobos': return 'px';
+    default: return '';
+  }
+};
+
+const setIntensityEffect = (value) => {
+  const currentEffect = uploadImage.dataset.id;
+  if (currentEffect === 'none') {
+    uploadImage.style.filter = null;
+    sliderContainer.classList.add('hidden');
+  } else {
+    sliderContainer.classList.remove('hidden');
+    uploadImage.style.filter = `${effectsToFilterName[currentEffect]}(${value}${getUnitsEffect(currentEffect)})`;
+  }
+};
+
+sliderElement.noUiSlider.on('update', () => {
+  const value = sliderElement.noUiSlider.get();
+  sliderValue.value = value;
+  setIntensityEffect(value);
+});
+
+effectsContainer.addEventListener('click', onEffectClick);
+
+export { resetEffect };
--- js/popup-upload.js
+++ js/popup-upload.js
@@ -1,5 +1,6 @@
 import { isEscKey } from './utils.js';
 import { resetScaleDefault, settingScale } from './scale-photo-upload.js';
+import { resetEffect } from './effects-photo-upload.js';
 
 const pageBody = document.body;
 const uploadContainer = document.querySelector('.img-upload__overlay');
@@ -36,6 +37,7 @@ function closePopup () {
 }
 
 function openPopup () {
+  resetEffect();
   settingScale();
   uploadContainer.classList.remove('hidden');
   pageBody.classList.add('modal-open');
